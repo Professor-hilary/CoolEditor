78e471cba74679a5627378c24b6219e0e7f13eb5
Okuonzi Hilary <professorhilary98@outlook.com>
2024-07-16 12:01:44 +0300

    Add files via upload

---
+++ CoolEditorApp.cpp
@@ -0,0 +1,34 @@
+/***************************************************************
+ * Name:      CoolEditorApp.cpp
+ * Purpose:   Code for Application Class
+ * Author:    prof hilary (profesorhilary@outlook.com)
+ * Created:   2024-07-14
+ * Copyright: prof hilary ()
+ * License:
+ **************************************************************/
+
+#include "wx_pch.h"
+#include "CoolEditorApp.h"
+
+//(*AppHeaders
+#include "CoolEditorMain.h"
+#include <wx/image.h>
+//*)
+
+IMPLEMENT_APP(CoolEditorApp);
+
+bool CoolEditorApp::OnInit()
+{
+    //(*AppInitialize
+    bool wxsOK = true;
+    wxInitAllImageHandlers();
+    if ( wxsOK )
+    {
+    	CoolEditorFrame* Frame = new CoolEditorFrame(0);
+    	Frame->Show();
+    	SetTopWindow(Frame);
+    }
+    //*)
+    return wxsOK;
+
+}
---
+++ CoolEditorMain.cpp
@@ -0,0 +1,271 @@
+/***************************************************************
+ * Name:      CoolEditorMain.cpp
+ * Purpose:   Code for Application Frame
+ * Author:    prof hilary (profesorhilary@outlook.com)
+ * Created:   2024-07-14
+ * Copyright: prof hilary ()
+ * License:
+ **************************************************************/
+
+#include "wx_pch.h"
+#include "CoolEditorMain.h"
+#include <wx/msgdlg.h>
+#include <wx/dir.h>
+#include <wx/filename.h>
+#include <wx/regex.h>
+#include <wx/richtext/richtextctrl.h>
+#include <wx/sstream.h>
+
+//(*InternalHeaders(CoolEditorFrame)
+#include <wx/intl.h>
+#include <wx/string.h>
+//*)
+
+//helper functions
+enum wxbuildinfoformat {
+    short_f, long_f
+};
+
+
+
+wxString wxbuildinfo( wxbuildinfoformat format ) {
+    wxString wxbuild( wxVERSION_STRING );
+
+    if ( format == long_f ) {
+        #if defined(__WXMSW__)
+        wxbuild << _T( "-Windows" );
+        #elif defined(__UNIX__)
+        wxbuild << _T( "-Linux" );
+        #endif
+
+        #if wxUSE_UNICODE
+        wxbuild << _T( "-Unicode build" );
+        #else
+        wxbuild << _T( "-ANSI build" );
+        #endif // wxUSE_UNICODE
+    }
+
+    return wxbuild;
+}
+
+//(*IdInit(CoolEditorFrame)
+const long CoolEditorFrame::ID_TEXTCTRL1 = wxNewId();
+const long CoolEditorFrame::ID_AUINOTEBOOK3 = wxNewId();
+const long CoolEditorFrame::ID_PANEL_HOME = wxNewId();
+const long CoolEditorFrame::ID_PANEL_EDIT = wxNewId();
+const long CoolEditorFrame::ID_PANEL_VIEW = wxNewId();
+const long CoolEditorFrame::ID_PANEL_LAYOUT = wxNewId();
+const long CoolEditorFrame::ID_PANEL_PROJECT = wxNewId();
+const long CoolEditorFrame::ID_PANEL_BUILD = wxNewId();
+const long CoolEditorFrame::ID_PANEL_DEBUG = wxNewId();
+const long CoolEditorFrame::ID_PANEL_SETTING = wxNewId();
+const long CoolEditorFrame::ID_PANEL_HELP = wxNewId();
+const long CoolEditorFrame::ID_AUINOTEBOOK4 = wxNewId();
+const long CoolEditorFrame::ID_DIRPICKERCTRL1 = wxNewId();
+const long CoolEditorFrame::ID_TREECTRL_FILELIST = wxNewId();
+const long CoolEditorFrame::ID_PANEL2 = wxNewId();
+const long CoolEditorFrame::ID_AUINOTEBOOK2 = wxNewId();
+const long CoolEditorFrame::ID_AUINOTEBOOK1 = wxNewId();
+const long CoolEditorFrame::ID_PANEL1 = wxNewId();
+const long CoolEditorFrame::ID_AUINOTEBOOK5 = wxNewId();
+const long CoolEditorFrame::ID_STATUSBAR1 = wxNewId();
+//*)
+
+BEGIN_EVENT_TABLE( CoolEditorFrame, wxFrame )
+    //(*EventTable(CoolEditorFrame)
+    //*)
+END_EVENT_TABLE()
+
+CoolEditorFrame::CoolEditorFrame( wxWindow* parent, wxWindowID id ) {
+    //(*Initialize(CoolEditorFrame)
+    wxBoxSizer* BoxSizer1;
+
+    Create( parent, wxID_ANY, _( "CoolEditor" ), wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE, _T( "wxID_ANY" ) );
+    SetClientSize( wxSize( 980, 450 ) );
+    AuiManager1 = new wxAuiManager( this, wxAUI_MGR_DEFAULT );
+    AuiNotebookConsolePane = new wxAuiNotebook( this, ID_AUINOTEBOOK3, wxDefaultPosition, wxSize( 0, 100 ), wxAUI_NB_DEFAULT_STYLE );
+    controlTextCtrl = new wxTextCtrl( AuiNotebookConsolePane, ID_TEXTCTRL1, _( "PS C:\\Users\\home> " ), wxDefaultPosition, wxDefaultSize, wxTE_PROCESS_ENTER | wxTE_AUTO_URL | wxTE_CHARWRAP | wxBORDER_NONE, wxDefaultValidator, _T( "ID_TEXTCTRL1" ) );
+    AuiNotebookConsolePane->AddPage( controlTextCtrl, _( "Page name" ) );
+    AuiManager1->AddPane( AuiNotebookConsolePane, wxAuiPaneInfo().Name( _T( "ConsolePane" ) ).DefaultPane().Caption( _( "Console" ) ).CaptionVisible().MinimizeButton().Position( 1 ).Bottom().DockFixed().BestSize( wxSize( 0, 100 ) ) );
+    AuiNotebookToolBar = new wxAuiNotebook( this, ID_AUINOTEBOOK4, wxDefaultPosition, wxSize( 0, 100 ), wxAUI_NB_WINDOWLIST_BUTTON | wxAUI_NB_CLOSE_ON_ALL_TABS | wxAUI_NB_DEFAULT_STYLE | wxFULL_REPAINT_ON_RESIZE );
+    PanelHome = new wxPanel( AuiNotebookToolBar, ID_PANEL_HOME, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL | wxCLIP_CHILDREN, _T( "ID_PANEL_HOME" ) );
+    PanelEdit = new wxPanel( AuiNotebookToolBar, ID_PANEL_EDIT, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T( "ID_PANEL_EDIT" ) );
+    PanelView = new wxPanel( AuiNotebookToolBar, ID_PANEL_VIEW, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T( "ID_PANEL_VIEW" ) );
+    PanelLayout = new wxPanel( AuiNotebookToolBar, ID_PANEL_LAYOUT, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T( "ID_PANEL_LAYOUT" ) );
+    PanelProject = new wxPanel( AuiNotebookToolBar, ID_PANEL_PROJECT, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T( "ID_PANEL_PROJECT" ) );
+    PanelBuild = new wxPanel( AuiNotebookToolBar, ID_PANEL_BUILD, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T( "ID_PANEL_BUILD" ) );
+    PanelDebug = new wxPanel( AuiNotebookToolBar, ID_PANEL_DEBUG, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T( "ID_PANEL_DEBUG" ) );
+    PanelSetting = new wxPanel( AuiNotebookToolBar, ID_PANEL_SETTING, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T( "ID_PANEL_SETTING" ) );
+    PanelHelp = new wxPanel( AuiNotebookToolBar, ID_PANEL_HELP, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T( "ID_PANEL_HELP" ) );
+    AuiNotebookToolBar->AddPage( PanelHome, _( "Home" ), true );
+    AuiNotebookToolBar->AddPage( PanelEdit, _( "Edit" ) );
+    AuiNotebookToolBar->AddPage( PanelView, _( "View" ) );
+    AuiNotebookToolBar->AddPage( PanelLayout, _( "Layout" ) );
+    AuiNotebookToolBar->AddPage( PanelProject, _( "Project" ) );
+    AuiNotebookToolBar->AddPage( PanelBuild, _( "Build" ) );
+    AuiNotebookToolBar->AddPage( PanelDebug, _( "Debug" ) );
+    AuiNotebookToolBar->AddPage( PanelSetting, _( "Settings" ) );
+    AuiNotebookToolBar->AddPage( PanelHelp, _( "Help" ) );
+    AuiManager1->AddPane( AuiNotebookToolBar, wxAuiPaneInfo().Name( _T( "ToolBar" ) ).DefaultPane().Caption( _( "Tool Bar" ) ).CaptionVisible( false ).PinButton().CloseButton( false ).Layer( 1 ).Top().DockFixed().BottomDockable( false ).LeftDockable( false ).RightDockable( false ).BestSize( wxSize( 0, 100 ) ) );
+    AuiNotebookManagement = new wxAuiNotebook( this, ID_AUINOTEBOOK2, wxDefaultPosition, wxSize( 150, 0 ), wxAUI_NB_DEFAULT_STYLE );
+    Panel2 = new wxPanel( AuiNotebookManagement, ID_PANEL2, wxPoint( -3, -50 ), wxDefaultSize, wxTAB_TRAVERSAL, _T( "ID_PANEL2" ) );
+    BoxSizer1 = new wxBoxSizer( wxVERTICAL );
+    DirPickerCtrl1 = new wxDirPickerCtrl( Panel2, ID_DIRPICKERCTRL1, _T( "C:\\Users\\home\\Documents" ), wxEmptyString, wxDefaultPosition, wxDefaultSize, wxDIRP_DIR_MUST_EXIST | wxDIRP_USE_TEXTCTRL | wxDIRP_SMALL, wxDefaultValidator, _T( "ID_DIRPICKERCTRL1" ) );
+    BoxSizer1->Add( DirPickerCtrl1, 0, wxALL | wxEXPAND, 5 );
+    treeCtrl = new wxTreeCtrl( Panel2, ID_TREECTRL_FILELIST, wxDefaultPosition, wxDefaultSize, wxTR_HAS_BUTTONS | wxTR_TWIST_BUTTONS | wxTR_DEFAULT_STYLE, wxDefaultValidator, _T( "ID_TREECTRL_FILELIST" ) );
+    BoxSizer1->Add( treeCtrl, 1, wxALL | wxEXPAND, 5 );
+    Panel2->SetSizer( BoxSizer1 );
+    BoxSizer1->Fit( Panel2 );
+    BoxSizer1->SetSizeHints( Panel2 );
+    AuiNotebookManagement->AddPage( Panel2, _( "Files" ) );
+    AuiManager1->AddPane( AuiNotebookManagement, wxAuiPaneInfo().Name( _T( "LeftPane" ) ).DefaultPane().Caption( _( "Management" ) ).CaptionVisible().Layer( 1 ).Left().BestSize( wxSize( 150, 0 ) ) );
+    AuiNotebookEditors = new wxAuiNotebook( this, ID_AUINOTEBOOK1, wxDefaultPosition, wxDefaultSize, wxAUI_NB_SCROLL_BUTTONS | wxAUI_NB_DEFAULT_STYLE );
+    AuiManager1->AddPane( AuiNotebookEditors, wxAuiPaneInfo().Name( _T( "MainViewPane" ) ).DefaultPane().Caption( _( "Editors" ) ).CaptionVisible().MinimizeButton().MaximizeButton().CloseButton( false ).Center().DockFixed().DestroyOnClose() );
+    AuiNotebookAside = new wxAuiNotebook( this, ID_AUINOTEBOOK5, wxDefaultPosition, wxSize( 150, 0 ), wxAUI_NB_DEFAULT_STYLE | wxFULL_REPAINT_ON_RESIZE );
+    Panel1 = new wxPanel( AuiNotebookAside, ID_PANEL1, wxPoint( 19, 270 ), wxDefaultSize, wxTAB_TRAVERSAL, _T( "ID_PANEL1" ) );
+    AuiNotebookAside->AddPage( Panel1, _( "Aside" ) );
+    AuiManager1->AddPane( AuiNotebookAside, wxAuiPaneInfo().Name( _T( "Aside" ) ).DefaultPane().Caption( _( "Side Pane" ) ).CaptionVisible().MinimizeButton().MaximizeButton().PinButton().CloseButton( false ).Right().BestSize( wxSize( 150, 0 ) ) );
+    AuiManager1->Update();
+    StatusBar1 = new wxStatusBar( this, ID_STATUSBAR1, 0, _T( "ID_STATUSBAR1" ) );
+    int __wxStatusBarWidths_1[1] = { -1 };
+    int __wxStatusBarStyles_1[1] = { wxSB_NORMAL };
+    StatusBar1->SetFieldsCount( 1, __wxStatusBarWidths_1 );
+    StatusBar1->SetStatusStyles( 1, __wxStatusBarStyles_1 );
+    SetStatusBar( StatusBar1 );
+    Center();
+
+    Connect( ID_TEXTCTRL1, wxEVT_COMMAND_TEXT_UPDATED, ( wxObjectEventFunction )&CoolEditorFrame::OncontrolTextCtrlTextEnter );
+    Connect( ID_DIRPICKERCTRL1, wxEVT_COMMAND_DIRPICKER_CHANGED, ( wxObjectEventFunction )&CoolEditorFrame::OnDirPickerCtrlDirChanged );
+    //*)
+
+    stc = new wxStyledTextCtrl( AuiNotebookEditors, wxID_ANY, wxDefaultPosition, wxSize( -1, -1 ), 0 );
+    //  stc->SetBackgroundColour( wxColour( 37, 44, 46 ) );
+    stc->SetCaret( new wxCaret( stc, 50, -1 ) );
+    stc->SetCaretLineVisible( true );
+    stc->SetCaretLineBackground( wxColour( 150, 150, 150 ) );
+    stc->SetMarginLeft( 20 );
+    stc->SetMarginType( 0, 0 );
+    setCppStyling();
+}
+
+void CoolEditorFrame::setCppStyling() {
+    stc->SetLexer(wxSTC_LEX_CPP);
+    AuiNotebookEditors->AddPage(stc, _("Styled Editor"));
+
+    // General settings
+    stc->StyleClearAll();
+
+    // Background color
+    wxColour backgroundColour(37, 44, 46);
+    stc->StyleSetBackground(wxSTC_STYLE_DEFAULT, backgroundColour);
+    stc->SetSelBackground(true, wxColour(60, 60, 60));
+    stc->SetSelForeground(true, wxColour(255, 255, 255));
+
+    // Default text color
+    stc->StyleSetForeground(wxSTC_STYLE_DEFAULT, wxColour(255, 255, 255));
+    stc->StyleSetBackground(wxSTC_STYLE_DEFAULT, backgroundColour);
+
+    // Comments
+    stc->StyleSetForeground(wxSTC_C_COMMENT, wxColour(0, 128, 0));           // Green
+    stc->StyleSetBackground(wxSTC_C_COMMENT, backgroundColour);
+    stc->StyleSetItalic(wxSTC_C_COMMENT, true);
+    stc->StyleSetForeground(wxSTC_C_COMMENTLINE, wxColour(0, 128, 0));       // Green
+    stc->StyleSetBackground(wxSTC_C_COMMENTLINE, backgroundColour);
+    stc->StyleSetItalic(wxSTC_C_COMMENTLINE, true);
+    stc->StyleSetForeground(wxSTC_C_COMMENTDOC, wxColour(0, 128, 0));        // Green
+    stc->StyleSetBackground(wxSTC_C_COMMENTDOC, backgroundColour);
+    stc->StyleSetItalic(wxSTC_C_COMMENTDOC, true);
+
+    // Preprocessor directives
+    stc->StyleSetForeground(wxSTC_C_PREPROCESSOR, wxColour(128, 128, 255));  // Light blue
+    stc->StyleSetBackground(wxSTC_C_PREPROCESSOR, backgroundColour);
+    stc->StyleSetBold(wxSTC_C_PREPROCESSOR, true);
+
+    // Strings
+    stc->StyleSetForeground(wxSTC_C_STRING, wxColour(255, 128, 0));          // Orange
+    stc->StyleSetBackground(wxSTC_C_STRING, backgroundColour);
+    stc->StyleSetForeground(wxSTC_C_CHARACTER, wxColour(255, 128, 0));       // Orange
+    stc->StyleSetBackground(wxSTC_C_CHARACTER, backgroundColour);
+
+    // Numbers
+    stc->StyleSetForeground(wxSTC_C_NUMBER, wxColour(255, 0, 255));          // Magenta
+    stc->StyleSetBackground(wxSTC_C_NUMBER, backgroundColour);
+
+    // Keywords
+    stc->StyleSetForeground(wxSTC_C_WORD, wxColour(0, 128, 255));            // Blue
+    stc->StyleSetBackground(wxSTC_C_WORD, backgroundColour);
+    stc->StyleSetBold(wxSTC_C_WORD, true);
+
+    // Classes, types
+    stc->StyleSetForeground(wxSTC_C_WORD2, wxColour(128, 0, 255));           // Purple
+    stc->StyleSetBackground(wxSTC_C_WORD2, backgroundColour);
+
+    // Operators
+    stc->StyleSetForeground(wxSTC_C_OPERATOR, wxColour(255, 255, 0));        // Yellow
+    stc->StyleSetBackground(wxSTC_C_OPERATOR, backgroundColour);
+    stc->StyleSetBold(wxSTC_C_OPERATOR, true);
+
+    // Load the file
+    stc->LoadFile(wxT("CoolEditorMain.cpp"));
+}
+
+CoolEditorFrame::~CoolEditorFrame() {
+    //(*Destroy(CoolEditorFrame)
+    //*)
+}
+
+void CoolEditorFrame::OnQuit( wxCommandEvent& event ) {
+    Close();
+}
+
+void CoolEditorFrame::OnAbout( wxCommandEvent& event ) {
+    wxString msg = wxbuildinfo( long_f );
+    wxMessageBox( msg, _( "Welcome to..." ) );
+}
+
+void CoolEditorFrame::OnDirPickerCtrlDirChanged( wxFileDirPickerEvent& event ) {
+    // Get the selected path from the DirPickerCtrl
+    wxString selectedPath = DirPickerCtrl1->GetPath();
+
+    // Clear existing items from the tree
+    treeCtrl->DeleteAllItems();
+
+    // Add the root item with the selected path
+    wxTreeItemId root = treeCtrl->AddRoot( selectedPath );
+
+    // Declare the recursive lambda function before use
+    std::function<void( const wxString&, wxTreeItemId )> PopulateSubdirectories;
+    PopulateSubdirectories = [&]( const wxString & path, wxTreeItemId parent ) {
+        wxDir dir( path );
+
+        if ( dir.IsOpened() ) {
+            wxString filename;
+            bool cont = dir.GetFirst( &filename );
+
+            while ( cont ) {
+                if ( filename != "." && filename != ".." ) {
+                    wxString filepath = wxFileName::DirName( path ).GetFullPath() + wxFileName::GetPathSeparator() + filename;
+
+                    if ( wxDirExists( filepath ) ) {
+                        wxTreeItemId child = treeCtrl->AppendItem( parent, filename, 0 );
+                        PopulateSubdirectories( filepath, child ); // Recursively add subdirectories
+
+                    } else {
+                        treeCtrl->AppendItem( parent, filename, 1 ); // Add files
+                    }
+                }
+
+                cont = dir.GetNext( &filename );
+            }
+        }
+    };
+
+    // Call the recursive function to populate the tree
+    PopulateSubdirectories( selectedPath, root );
+}
+
+void CoolEditorFrame::OncontrolTextCtrlTextEnter( wxCommandEvent& event ) {
+    // int numOfCaracters = controlTextCtrl->GetSize();
+    std::cout << controlTextCtrl->GetLabel() << std::endl;
+}
+
